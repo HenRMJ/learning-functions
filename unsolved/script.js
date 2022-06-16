e760c5dec9033f69f8559604b464ae66883e1562
HenRMJ <henryrmejia96@gmail.com>
2022-06-15 19:08:36 -0700

    solved

--- unsolved/script.js
+++ unsolved/script.js
@@ -2,12 +2,17 @@
 // 1. Write a function that logs 'Hello World!' to the console
 // Write your code below this line (* don't forget to call your function)
 
+const hello = () => {
+    console.log("Hello World!");
+}
+
+hello();
 
 // 2. Write a function to calulate the area of an object
 // Formula: area = length * width
 // Write your code below this line
 
-
+const area = (length, width) => length * width;
 
 console.log(`area = ${area(10,20)}`); // Function is called within console.log
 
@@ -15,7 +20,7 @@ console.log(`area = ${area(10,20)}`); // Function is called within console.log
 // Formula: length * width * height
 // Write your code below this line
 
-
+const volume = (length, width, height) => length * width * height;
 
 console.log(`volume = ${volume(10, 20, 30)}`);
 
@@ -23,8 +28,14 @@ console.log(`volume = ${volume(10, 20, 30)}`);
 //Football Scoring System: touchdown = 6, extraPoint = 1, twoPoint = 2, fieldGoal = 3
 // Write your code below this line
 
-
+const superBowlScore = (touchdown, extraPoint, twoPoint, fieldGoal) => (touchdown * 6) + extraPoint + (twoPoint * 2) + (fieldGoal * 3);
 
 console.log(`SuperBowl score prediction: ${superBowlScore(3, 3, 0, 5)}`);
 
 // Write your Bonus code below this line
+
+const teamVersus = (teamOne, scoreOne, teamTwo, scoreTwo) => {
+    console.log(`${teamOne}: ${scoreOne} ${teamTwo}: ${scoreTwo}`);
+}
+
+teamVersus('Bengals', superBowlScore(3,3,0,5), 'Rams', superBowlScore(0,0,0,0));
